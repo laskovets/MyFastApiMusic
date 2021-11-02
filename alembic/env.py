01b0876f4531982de87faf3f6ae26899d7e4a424
Alexander Laskovets <a.laskovets@mail.ru>
2021-11-02 10:25:52 +0300

    update alembic connection with postgres

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.db.base_class import Base
+from app.db.base import Base
 from app.db.session import SQLALCHEMY_DATABASE_URI
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ app/db/base.py
@@ -0,0 +1,4 @@
+from app.db.base_class import Base
+from app.models.author import Author
+from app.models.album import Album
+from app.models.track import Track
