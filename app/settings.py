56e9113e3164261982c17be8cc6186a3375f1b95
Alexander Laskovets <a.laskovets@mail.ru>
2021-11-23 17:50:33 +0300

    fix error in settings

--- app/settings.py
+++ app/settings.py
@@ -5,4 +5,4 @@ import os
 load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 ALGORITHM = os.environ.get('ALGORITHM', 'HS256')
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 30)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
